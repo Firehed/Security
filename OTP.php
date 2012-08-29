214edfbb67c91d4bf957373a0a05e6741c22393e
Eric Stern <firehed@gmail.com>
2012-08-29 05:06:31 +0000

    add support for other HMAC algorithms per RFC spec

--- OTP.php
+++ OTP.php
@@ -12,13 +12,14 @@ class OTP {
 	 * [@param $step = 30] Time step in seconds (section 4.1)
 	 * [@param $t0 = 0] Unix time to start counting steps (section 4.1) (note: positive and negative $t0 in $step increments may be used to check the next and previous codes respectively, which can help address clock drift)
 	 * [@param $digits = 6] Length of the output code
+	 * [@param $algorithm = 'sha1'] HMAC algorithm - sha1, sha256, and sha512 permitted
 	 * @return string n-character numeric code
 	 */
-	public static function TOTP($key, $step = 30, $t0 = 0, $digits = 6) {
+	public static function TOTP($key, $step = 30, $t0 = 0, $digits = 6, $algorithm = 'sha1') {
 		$counterInt = floor(($_SERVER['REQUEST_TIME'] - $t0) / $step);
 		$C = pack('N*', $counterInt & 0xFFFFFFFF00000000) 
 		   . pack('N*', $counterInt & 0x00000000FFFFFFFF);
-		return self::HOTP($key, $C, $digits);
+		return self::HOTP($key, $C, $digits, $algorithm);
 	}
 	/**
 	 * HMAC-Based One-Time Password Algorithm
@@ -26,15 +27,15 @@ class OTP {
 	 * @param $binaryKey binary-formatted key 
 	 * @param $binaryCounter binary-formatted counter
 	 * [@param $digits = 6] Length of the output code
+	 * [@param $algorithm = 'sha1'] HMAC algorithm - sha1, sha256, and sha512 permitted
 	 * @return string n-character numeric code
 	 */
-	public static function HOTP($binaryKey, $binaryCounter, $digits = 6) {
+	public static function HOTP($binaryKey, $binaryCounter, $digits = 6, $algorithm = 'sha1') {
 		if ($digits < 6) {
 			throw new Exception('RFC4226 requires a minimum of six-digit output');
 		}
-		$alg = 'sha1';
 
-		$hash = hash_hmac($alg, $binaryCounter, $binaryKey, true);
+		$hash = hash_hmac($algorithm, $binaryCounter, $binaryKey, true);
 		$offset = ord(substr($hash, -1)) & 0xF;
 		$truncated = substr($hash, $offset, 4);
 		extract(unpack('Nnum', $truncated));
