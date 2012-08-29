176e109a6b1d638489d4d6d843f2c4295e3d78c3
Eric Stern <firehed@gmail.com>
2012-08-29 05:46:34 +0000

    Adjust variable names and commenting for added clarity

--- OTP.php
+++ OTP.php
@@ -4,11 +4,16 @@ namespace Firehed\Security;
 
 use Exception;
 
+// todo:
+// * throttling
+// * resynchronization
+// * slow comparison
+
 class OTP {
 	/**
 	 * Time-based One-Time Password Algorithm
 	 * @see RFC 6238
-	 * @param $key shared secret, in binary format (note: Google Authenticator's keys are base32-encoded, and must be decoded before being passed in)
+	 * @param $key shared secret, treated as binary (note: Google Authenticator's keys are base32-encoded, and must be decoded before being passed in)
 	 * [@param $step = 30] Time step in seconds (section 4.1)
 	 * [@param $t0 = 0] Unix time to start counting steps (section 4.1) (note: positive and negative $t0 in $step increments may be used to check the next and previous codes respectively, which can help address clock drift)
 	 * [@param $digits = 6] Length of the output code
@@ -17,25 +22,28 @@ class OTP {
 	 */
 	public static function TOTP($key, $step = 30, $t0 = 0, $digits = 6, $algorithm = 'sha1') {
 		$counterInt = floor(($_SERVER['REQUEST_TIME'] - $t0) / $step);
-		$C = pack('N*', $counterInt & 0xFFFFFFFF00000000) 
-		   . pack('N*', $counterInt & 0x00000000FFFFFFFF);
-		return self::HOTP($key, $C, $digits, $algorithm);
+		$counter = pack('N*', $counterInt & 0xFFFFFFFF00000000) 
+		         . pack('N*', $counterInt & 0x00000000FFFFFFFF);
+		return self::HOTP($key, $counter, $digits, $algorithm);
 	}
 	/**
 	 * HMAC-Based One-Time Password Algorithm
 	 * @see RFC 4226
-	 * @param $binaryKey binary-formatted key 
-	 * @param $binaryCounter binary-formatted counter
+	 * @param $key shared secret, treated as binary 
+	 * @param $counter 8-byte counter
 	 * [@param $digits = 6] Length of the output code
 	 * [@param $algorithm = 'sha1'] HMAC algorithm - sha1, sha256, and sha512 permitted
 	 * @return string n-character numeric code
 	 */
-	public static function HOTP($binaryKey, $binaryCounter, $digits = 6, $algorithm = 'sha1') {
+	public static function HOTP($key, $counter, $digits = 6, $algorithm = 'sha1') {
 		if ($digits < 6) {
 			throw new Exception('RFC4226 requires a minimum of six-digit output');
 		}
+		if (strlen($counter) != 8) {
+			throw new Exception('Counter must be 8 bytes long');
+		}
 
-		$hash = hash_hmac($algorithm, $binaryCounter, $binaryKey, true);
+		$hash = hash_hmac($algorithm, $counter, $key, true);
 		$offset = ord(substr($hash, -1)) & 0xF;
 		$noMSB = ((ord($hash[$offset + 0]) & 0x7F) << 24)
 		       | ((ord($hash[$offset + 1]) & 0xFF) << 16)
