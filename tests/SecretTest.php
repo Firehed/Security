Firehed/Security

commit 3cf00fc73f0fb17a1d1b4624b06b57b1be21ea14

Avoid mbstring.func_overload issues

src/Secret.php
@@ -59,9 +59,10 @@ final class Secret {
      */
     private function mask(string $string, string $noise): string {
         $result = '';
-        for ($ii = 0; $ii < strlen($string); $ii++) {
+        $len = mb_strlen($string, '8bit');
+        for ($ii = 0; $ii < $len; $ii++) {
             $s = $string[$ii];
-            $n = $noise[$ii % strlen($noise)];
+            $n = $noise[$ii % $len];
 
             $result .= chr(ord($s) ^ ord($n));
         }

commit ff8ef13d53301079c2d47164767c2a336ac88b81

Fix incorrect mod operation introduced in 3cf00fc

src/Secret.php
@@ -60,9 +60,10 @@ final class Secret {
     private function mask(string $string, string $noise): string {
         $result = '';
         $len = mb_strlen($string, '8bit');
+        $noise_len = mb_strlen($noise, '8bit');
         for ($ii = 0; $ii < $len; $ii++) {
             $s = $string[$ii];
-            $n = $noise[$ii % $len];
+            $n = $noise[$ii % $noise_len];
 
             $result .= chr(ord($s) ^ ord($n));
         }

tests/SecretTest.php
@@ -71,4 +71,17 @@ class SecretTest extends \PHPUnit_Framework_TestCase
         $this->assertNotContains($test_string, $dumped,
             'var_dump revealed the secret');
     }
+
+    /**
+     * @covers ::__construct
+     * @covers ::reveal
+     */
+    public function testMaskingStringLongerThanNoiseLength()
+    {
+        $noise = SecretKey::getKey();
+        $noise_length = mb_strlen($noise, '8bit');
+        $string = str_repeat('asdf', $noise_length);
+        $secret = new Secret($string);
+        $this->assertSame($string, $secret->reveal(), 'Secret was destroyed');
+    }
 }